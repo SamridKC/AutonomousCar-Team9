67e9c7213d7af02ca845a23e23c04b920739714b
EvanDague <Evan.Dague@ucdenver.edu>
2018-03-26 15:18:15 -0600

    Add files via upload
    Fixed Code

--- Robot_Motor.ino
+++ Robot_Motor.ino
@@ -1,49 +1,160 @@
 #include <Wire.h>
 #include <MPU9250.h>
 
-MPU9250 myIMU1;
+bool testing = true;
 
+MPU9250 myIMU;
+int delayTime = 100;
+
+int speedpinA = 9;
+int speedpinB = 10;
+int pinI1 = 8;
+int pinI2 = 11;
+int pinI3 = 12;
+int pinI4 = 13;
+int spead = 80;
+
+void forward(){
+  analogWrite(speedpinA,spead);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead);
+  digitalWrite(pinI4,LOW);//turn DC Motor B move clockwise
+  digitalWrite(pinI3,HIGH);
+  digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+  digitalWrite(pinI1,HIGH);
+
+  delay(1000);
+  stop();
+}
+void backward(){
+  analogWrite(speedpinA,spead);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead);
+  digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+  digitalWrite(pinI3,LOW);
+  digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+  digitalWrite(pinI1,LOW);
+
+  delay(1000);
+  stop();
+}
+
+void turnLeft(){
+  float angle = 0;
+  analogWrite(speedpinA,spead);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead);
+  digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+  digitalWrite(pinI3,LOW);
+  digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+  digitalWrite(pinI1,HIGH);
+     
+  while(angle < 90){
+    myIMU.readGyroData(myIMU.accelCount);
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.gRes -myIMU.gyroBias[2];
+
+    if(!(myIMU.ay < 1.5 && myIMU.ay > -1.5)){
+      angle+= myIMU.ay / (1000 / delayTime);
+    }
+
+    if(testing)
+      Serial.println(angle);
+
+    delay(delayTime);
+  }
+  stop(); 
+}
+void turnRight(){
+  float angle = 0;
+  analogWrite(speedpinA,spead);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead);
+  digitalWrite(pinI4,LOW);//turn DC Motor B move clockwise
+  digitalWrite(pinI3,HIGH);
+  digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+  digitalWrite(pinI1,LOW);
+     
+  while(angle > -90){
+    myIMU.readGyroData(myIMU.accelCount);
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.gRes -myIMU.gyroBias[2];
+
+    if(!(myIMU.ay < 1.5 && myIMU.ay > -1.5)){
+      angle+= myIMU.ay / (1000 / delayTime);
+    }
+     
+    if(testing){
+      Serial.println(angle);
+    }
+
+    delay(delayTime);
+  }
+  stop(); 
+}
+void halfTurn(){
+  float angle = 0;
+  analogWrite(speedpinA,spead);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead);
+  digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+  digitalWrite(pinI3,LOW);
+  digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+  digitalWrite(pinI1,HIGH);
+     
+  while(angle < 180){
+    myIMU.readGyroData(myIMU.accelCount);
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.gRes -myIMU.gyroBias[2];
+
+    if(!(myIMU.ay < 1.5 && myIMU.ay > -1.5)){
+      angle += myIMU.ay / (1000 / delayTime);
+    }
+     
+    if(testing)
+      Serial.println(angle);
+
+    delay(delayTime);
+  }
+  stop(); 
+}
+
+void stop(){
+  digitalWrite(speedpinA,LOW);//input a simulation value to set the speed
+  digitalWrite(speedpinB,LOW);
+}
 
 void setup() {
   Serial.begin(115200);
+  
+  pinMode(speedpinA, OUTPUT);
+  pinMode(speedpinB, OUTPUT);
+  pinMode(pinI1, OUTPUT);
+  pinMode(pinI2, OUTPUT);
+  pinMode(pinI3, OUTPUT);
+  pinMode(pinI4, OUTPUT);
+  
   Wire.begin();
 
-  myIMU1.initMPU9250();
-
- // myIMU1.writeByte(myIMU1.sensorAddress, SMPLRT_DIV, 0);
-
-  myIMU1.getAres();
-  myIMU1.getGres();
+  myIMU.initMPU9250();
+  myIMU.getAres();
+  myIMU.getGres();
 }
 
-float angle = 0;
-
-void loop() {
-  
-
-  
-  myIMU1.readGyroData(myIMU1.accelCount);
-  myIMU1.ax = (float)myIMU1.accelCount[0] * myIMU1.gRes -myIMU1.gyroBias[0];
-  myIMU1.ay = (float)myIMU1.accelCount[1] * myIMU1.gRes -myIMU1.gyroBias[1];
-  myIMU1.az = (float)myIMU1.accelCount[2] * myIMU1.gRes -myIMU1.gyroBias[2];
-
-  if(!(myIMU1.ay < 1 && myIMU1.ay > -1)){
-    angle+= myIMU1.ay / 10;
-  }
-  if(angle < 0)
-    angle += 360;
-  if(angle > 360)
-    angle -= 360;
-  
-  Serial.println(angle);
-  
-  /*
-  Serial.print(myIMU1.ax);
-  Serial.print('\t');
-  Serial.print(myIMU1.ay);
-  Serial.print('\t');
-  Serial.println(myIMU1.az);
-  */
-  delay(100);
+void loop() { 
+  //*
+  turnRight();
+  delay(3000);
+  //*/
+  turnLeft();
+  delay(1000);
+  //*/
+  halfTurn();
+  delay(1000);
+  //*/
+  forward();
+  delay(1000);
+  //*/
+  backward();
+  delay(1000);
+  //*/
 }
 
