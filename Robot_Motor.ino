8aee5448f1faa1970e580b91a185db7f0899f842
EvanDague <Evan.Dague@ucdenver.edu>
2018-04-24 14:43:12 -0600

    Complete Code

--- Robot_Motor.ino
+++ Robot_Motor.ino
@@ -1,18 +1,31 @@
 #include <Wire.h>
 #include <MPU9250.h>
-#include <SparkFun_VL6180X.h>
+#include <VL6180X.h>
 
-VL6180xIdentification identification;
-#define VL6180X_ADDRESS_1 0x50
-VL6180x frontSensor(VL6180X_ADDRESS_1);
-#define VL6180X_ADDRESS_2 0x51
-VL6180x rightSensor(VL6180X_ADDRESS_2);
+bool angleTesting = true;
+bool turnTesting = true;
 
+#define RANGE 1
 
-bool testing = true;
+/* List of adresses for each sensor - after reset the address can be configured */
+#define address0 0x22
+#define address1 0x20
+#define address2 0x24
+
+/* These Arduino pins must be wired to the IO0 pin of VL6180x */
+int enablePin0 = 3;
+int enablePin1 = 2;
+int enablePin2 = 4;
+
+/* Create a new instance for each sensor */
+VL6180X sensor0;
+VL6180X sensor1;
+VL6180X sensor2;
 
 MPU9250 myIMU;
-int delayTime = 100;
+int delayTime = 25;
+float turnspeed = 1;
+float turnBuffer = .9;
 
 int speedpinA = 9;
 int speedpinB = 10;
@@ -20,20 +33,19 @@ int pinI1 = 8;
 int pinI2 = 11;
 int pinI3 = 12;
 int pinI4 = 13;
-int spead = 80;
+int spead = 150;
 
 void forward(){
-  analogWrite(speedpinA,spead);//input a simulation value to set the speed
-  analogWrite(speedpinB,spead);
+  analogWrite(speedpinA, spead);//input a simulation value to set the speed
+  analogWrite(speedpinB, spead);
   digitalWrite(pinI4,LOW);//turn DC Motor B move clockwise
   digitalWrite(pinI3,HIGH);
   digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
   digitalWrite(pinI1,HIGH);
-
-  delay(1000);
-  stop();
 }
+
 void backward(){
+  stopMotors();
   analogWrite(speedpinA,spead);//input a simulation value to set the speed
   analogWrite(speedpinB,spead);
   digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
@@ -41,20 +53,21 @@ void backward(){
   digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
   digitalWrite(pinI1,LOW);
 
-  delay(1000);
-  stop();
+  delay(250);
+  stopMotors();
 }
 
-void turnLeft(){
+float turnLeft(){
+  stopMotors();
   float angle = 0;
-  analogWrite(speedpinA,spead);//input a simulation value to set the speed
-  analogWrite(speedpinB,spead);
+  analogWrite(speedpinA,spead * turnspeed);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead * turnspeed);
   digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
   digitalWrite(pinI3,LOW);
   digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
   digitalWrite(pinI1,HIGH);
      
-  while(angle < 90){
+  while(angle < 90*turnBuffer){
     myIMU.readGyroData(myIMU.accelCount);
     myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
     myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
@@ -64,23 +77,24 @@ void turnLeft(){
       angle+= myIMU.ay / (1000 / delayTime);
     }
 
-    if(testing)
+    if(angleTesting)
       Serial.println(angle);
 
     delay(delayTime);
   }
-  stop(); 
+  stopMotors(); 
 }
-void turnRight(){
+float turnRight(){
+  stopMotors();
   float angle = 0;
-  analogWrite(speedpinA,spead);//input a simulation value to set the speed
-  analogWrite(speedpinB,spead);
+  analogWrite(speedpinA,spead * turnspeed);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead * turnspeed);
   digitalWrite(pinI4,LOW);//turn DC Motor B move clockwise
   digitalWrite(pinI3,HIGH);
   digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
   digitalWrite(pinI1,LOW);
      
-  while(angle > -90){
+  while(angle > -90*turnBuffer){
     myIMU.readGyroData(myIMU.accelCount);
     myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
     myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
@@ -90,24 +104,25 @@ void turnRight(){
       angle+= myIMU.ay / (1000 / delayTime);
     }
      
-    if(testing){
+    if(angleTesting){
       Serial.println(angle);
     }
 
     delay(delayTime);
   }
-  stop(); 
+  stopMotors(); 
 }
-void halfTurn(){
+float halfTurn(){
+  stopMotors();
   float angle = 0;
-  analogWrite(speedpinA,spead);//input a simulation value to set the speed
-  analogWrite(speedpinB,spead);
+  analogWrite(speedpinA,spead * turnspeed);//input a simulation value to set the speed
+  analogWrite(speedpinB,spead * turnspeed);
   digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
   digitalWrite(pinI3,LOW);
   digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
   digitalWrite(pinI1,HIGH);
      
-  while(angle < 180){
+  while(angle < 180*turnBuffer){
     myIMU.readGyroData(myIMU.accelCount);
     myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
     myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
@@ -117,21 +132,123 @@ void halfTurn(){
       angle += myIMU.ay / (1000 / delayTime);
     }
      
-    if(testing)
+    if(angleTesting)
       Serial.println(angle);
 
     delay(delayTime);
   }
-  stop(); 
+  stopMotors();
 }
 
-void stop(){
+float degreeTurn(float turnAmount){
+  stopMotors();
+  float angle = 0;
+  if(turnAmount > 0){
+    analogWrite(speedpinA,spead * turnspeed);//input a simulation value to set the speed
+    analogWrite(speedpinB,spead * turnspeed);
+    digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+    digitalWrite(pinI3,LOW);
+    digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+    digitalWrite(pinI1,HIGH);
+  }else{
+    analogWrite(speedpinA,spead * turnspeed);//input a simulation value to set the speed
+    analogWrite(speedpinB,spead * turnspeed);
+    digitalWrite(pinI4,LOW);//turn DC Motor B move clockwise
+    digitalWrite(pinI3,HIGH);
+    digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+    digitalWrite(pinI1,LOW);
+  }
+     
+    while(turnAmount > 0 ? angle < turnAmount*turnBuffer : angle > turnAmount*turnBuffer){
+      myIMU.readGyroData(myIMU.accelCount);
+      myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
+      myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
+      myIMU.az = (float)myIMU.accelCount[2] * myIMU.gRes -myIMU.gyroBias[2];
+  
+      if(!(myIMU.ay < 1.5 && myIMU.ay > -1.5)){
+        angle += myIMU.ay / (1000 / delayTime);
+      }
+       
+      if(angleTesting)
+        Serial.println(angle);
+
+      delay(delayTime);
+    }
+  stopMotors();
+}
+
+void stopMotors(){
+  if(turnTesting)
+       Serial.println("Stopping Motors");
+  
   digitalWrite(speedpinA,LOW);//input a simulation value to set the speed
   digitalWrite(speedpinB,LOW);
 }
 
+void VL6180Xsetup(){
+  // Sensor0
+  Serial.println("Start Sensor 0");
+  digitalWrite(enablePin0, HIGH);
+  delay(50);
+  sensor0.init();
+  sensor0.configureDefault();
+  sensor0.setAddress(address0);
+  Serial.println(sensor0.readReg(0x212),HEX); // read I2C address
+  sensor0.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+  sensor0.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+  sensor0.setTimeout(500);
+  sensor0.stopContinuous();
+  sensor0.setScaling(RANGE); // configure range or precision 1, 2 oder 3 mm
+  delay(300);
+  sensor0.startInterleavedContinuous(100);
+  delay(100);
+  
+  // Sensor1
+  Serial.println("Start Sensor 1");
+  digitalWrite(enablePin1, HIGH);
+  delay(50);
+  sensor1.init();
+  sensor1.configureDefault();
+  sensor1.setAddress(address1);
+  Serial.println(sensor1.readReg(0x212),HEX);
+  sensor1.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+  sensor1.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+  sensor1.setTimeout(500);
+  sensor1.stopContinuous();
+    sensor1.setScaling(RANGE);
+  delay(300);
+  sensor1.startInterleavedContinuous(100);
+  delay(100);
+  
+  //* Sensor2
+  Serial.println("Start Sensor 2");
+  digitalWrite(enablePin2, HIGH);
+  delay(50);
+  sensor2.init();
+  sensor2.configureDefault();
+  sensor2.setAddress(address2);
+  Serial.println(sensor2.readReg(0x212),HEX);
+  sensor2.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+  sensor2.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+  sensor2.setTimeout(500);
+  sensor2.stopContinuous();
+  sensor2.setScaling(RANGE);
+  delay(300);
+  sensor2.startInterleavedContinuous(100);
+  delay(1000);
+}
+
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(enablePin0,OUTPUT);
+  pinMode(enablePin1,OUTPUT);
+  pinMode(enablePin2,OUTPUT);
+  
+  digitalWrite(enablePin0, LOW);
+  digitalWrite(enablePin1, LOW);
+  digitalWrite(enablePin2, LOW);
   
   pinMode(speedpinA, OUTPUT);
   pinMode(speedpinB, OUTPUT);
@@ -142,50 +259,112 @@ void setup() {
   
   Wire.begin();
 
-  frontSensor.changeAddress(0x29, VL6180X_ADDRESS_1);
-  rightSensor.changeAddress(0x29, VL6180X_ADDRESS_2);
-  if(frontSensor.VL6180xInit() != 0){
-    Serial.println("FAILED TO INITALIZE FRONT SENSOR"); //Initialize device and check for errors
-  };
-  if(rightSensor.VL6180xInit() != 0){
-    Serial.println("FAILED TO INITALIZE RIGHT SENSOR"); //Initialize device and check for errors
-  };
-
-  frontSensor.VL6180xDefautSettings(); //Load default settings to get started.
-  rightSensor.VL6180xDefautSettings(); //Load default settings to get started.
+  VL6180Xsetup();
   
   myIMU.initMPU9250();
   myIMU.getAres();
   myIMU.getGres();
+
+  forward();
+  delay(2000);
 }
 
-void loop() { 
-  /*
-  turnRight();
-  delay(3000);
-  ///
-  turnLeft();
-  delay(1000);
-  ///
-  halfTurn();
-  delay(1000);
-  ///
-  forward();
-  delay(1000);
-  ///
-  backward();
-  delay(1000);
-  //*/
-  myIMU.readGyroData(myIMU.accelCount);
-  myIMU.ax = (float)myIMU.accelCount[0] * myIMU.gRes -myIMU.gyroBias[0];
-  myIMU.ay = (float)myIMU.accelCount[1] * myIMU.gRes -myIMU.gyroBias[1];
-  myIMU.az = (float)myIMU.accelCount[2] * myIMU.gRes -myIMU.gyroBias[2];
-  Serial.println(myIMU.ay);
+short forwardRead = 0, rightRead = 0, leftRead = 0;
+void ReadSensors(){
+  int forward[5], right[5], left[5];
+  for(int i = 0; i < 5; i++){
+    forward[i] = sensor0.readRangeContinuousMillimeters();
+    right[i] = sensor1.readRangeContinuousMillimeters();
+    left[i] = sensor2.readRangeContinuousMillimeters();
+  }
+  forwardRead = (forward[0] + forward[1] + forward[2] + forward[3] + forward[4]) / 5;
+  rightRead = (right[0] + right[1] + right[2] + right[3] + right[4]) / 5;
+  leftRead = (left[0] + left[1] + left[2] + left[3] + left[4]) / 5;
+}
 
-  Serial.print("Front distance measured (mm) = ");
-  Serial.println(frontSensor.getDistance() ); 
-  Serial.print("Right distance measured (mm) = ");
-  Serial.println(rightSensor.getDistance() ); 
+int distance = 250;
+int forwardDistance = 150;
+int adjustDistance = 140;
+int waitTime = 900;
+
+void loop() {
+  //*
+  ReadSensors();
+
+  if(forwardRead > forwardDistance){                          // If nothing close enough in front of you
+    if(rightRead > distance && leftRead > distance){   // and if there's no walls next to you
+    }                                                // then it's an intersection ,and go forward still.
+    else if(rightRead > distance){                   // else if there is no wall to the right, but there was one to the left
+      if(turnTesting)
+        Serial.println("Right turn");
+      delay(waitTime);
+      stopMotors();
+      ReadSensors();
+      if(forwardRead > forwardDistance && rightRead > distance && leftRead < distance){
+        turnRight();                                // and turn right.
+      }
+    }
+    else if(leftRead > distance){                 // if there is a wall to the right, an no wall to the left
+      if(turnTesting)
+        Serial.println("Left turn");
+      delay(waitTime);
+      stopMotors();
+      ReadSensors();
+      if(forwardRead > forwardDistance && rightRead < distance && leftRead > distance){
+        turnLeft();                                // and turn right.
+      }                             // and turn left
+    }                                       
+    else{                                         // else if you're going forward
+      if(rightRead > adjustDistance){                         // check if the right wall is too close
+         if(turnTesting)
+            Serial.println("Adjusting left");
+         degreeTurn(-5);
+       }
+       if(leftRead > adjustDistance){                    // else if the left wall is too close
+          if(turnTesting)
+            Serial.println("Adjusting right");
+         degreeTurn(5);
+       }
+    }
+  }
+  else if(forwardRead <= forwardDistance){                    // Else if there is a wall in front of you    
+    stopMotors();  
+    backward();
+    if(rightRead > distance){                        // and there is no wall to the right
+      if(turnTesting)
+        Serial.println("Right turn");
+      delay(waitTime);
+      turnRight();                                // and turn right,
+    }                                             
+    else if(leftRead > distance){                   // else if there is a wall to the right, bu no wall to the left
+      if(turnTesting)
+        Serial.println("Left turn");
+      delay(waitTime);
+      turnLeft();                                 // and turn left.
+    }                                      
+    else{                                         // else there's no path to go
+      if(turnTesting)
+        Serial.println("Half turn");
+      halfTurn();                                 // and turn around
+    }
+  }
   delay(100);
+  forward();                // go forward after all the logic of turning
+  delay(50);
+  //*/
+  /*
+  turnLeft();
+  delay(2000);
+  turnRight();
+  delay(2000);
+  degreeTurn(45);
+  delay(2000);
+  degreeTurn(-45);
+  delay(2000);
+  degreeTurn(10);
+  delay(2000);
+  degreeTurn(-10);
+  delay(2000);
+  //*/
 }
 
